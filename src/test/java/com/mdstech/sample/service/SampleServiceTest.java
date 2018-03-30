4f34251e368d20dbbb6e7814cf3b9f7f0fb2cd9a
Srini Maddineni <srini@mdstechinc.com>
2018-03-31 02:59:25 +0530

    Integrated with sonar maven plugin using jacoco

--- src/test/java/com/mdstech/sample/service/SampleServiceTest.java
+++ src/test/java/com/mdstech/sample/service/SampleServiceTest.java
@@ -1,9 +1,11 @@
 package com.mdstech.sample.service;
 
 import com.mdstech.sample.config.ApplicationConfiguration;
+import com.mdstech.sample.vo.SampleVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -22,5 +24,11 @@ public class SampleServiceTest {
     @Test
     public void testSpringIitialization() {
         assertThat("Spring Context initialized", is(sampleService), notNullValue() );
+    }
+
+    @Test
+    public void testPagebaleQuery() {
+        Page<SampleVO> page = sampleService.getPage("test1", 1);
+        assertThat("Should contain more pages", page.isLast(), is(false));
     }
 }
